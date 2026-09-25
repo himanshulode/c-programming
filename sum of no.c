e48b6790348269c38d3efb4e3413cb35bc88bd87
himanshulode <himanshulode37@gmail.com>
2026-09-25 23:33:24 +0530

    Add files via upload

---
+++ sum of no.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+
+    printf("Enter the two number :");
+    scanf("%d %d",&a,&b);
+    sum = a + b;
+    printf("Sum of two number :%d\n",sum);
+    printf("Product of two number :%d\n",a*b);
+    printf("Substraction of two number :%d\n",a-b);
+    printf("division :%d\n",a/b);
+
+
+    return 0;
+}
